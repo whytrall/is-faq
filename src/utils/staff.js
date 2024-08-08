fbbd984151c43efd7c636170870ceadc35430d27
Nikolay Andreev <bakasaru@list.ru>
2024-08-09 01:08:01 +0300

    feat: replace faculty staff list with nice cards

---
+++ src/utils/staff.js
@@ -0,0 +1,44 @@
+export const mainStaff = [
+    {
+        name: "Маятин Александр Владимирович",
+        email: "mayatin@itmo.ru",
+        room: "2434",
+        avatar: "https://photo.itmo.su/avatar/ab7389a33ecd63e4bd6c96ddd9551d73c791806f/cover/360/360/",
+        position: "заместитель декана, руководитель образовательной программы",
+    },
+    {
+        name: "Зубок Дмитрий Алексндрович",
+        email: "zubok@itmo.ru",
+        room: "2125",
+        avatar: "https://photo.itmo.su/avatar/47081a1ee72f479a234cb5fc09a73262c87099c2/cover/360/360/",
+        position: "заместитель декана, руководитель практики на образовательной программе",
+    },
+    {
+        name: "Страдина Марина Владимировна",
+        email: "stradina@itmo.ru",
+        room: "2434",
+        avatar: "https://photo.itmo.su/avatar/ddafc488-c833-4fb0-8947-ea1a6dbb59f6/cover/360/360/",
+        position: "заместитель декана, контактное лицо по вопросам ППА",
+    },
+    {
+        name: "Харченко Татьяна Владимировна",
+        email: "kharchenko@itmo.ru",
+        room: "2235",
+        avatar: "https://photo.itmo.su/avatar/ca3a65bc-fe1c-469f-bf38-dbf342660b3d/cover/360/360/",
+        position: "заместитель декана, контактное лицо по вопросам переводов",
+    },
+    {
+        name: "Найденкова Марина Владимировна",
+        email: "naidenkova@itmo.ru",
+        room: "2139",
+        avatar: "https://my.itmo.ru/img/personalities/default-avatar-female.svg",
+        position: "специалист по УМР, сотрудник деканата",
+    },
+    {
+        name: "Митюхина Елена Анатольевна",
+        email: "eamitiukhina@itmo.ru",
+        room: "2139",
+        avatar: "https://my.itmo.ru/img/personalities/default-avatar-female.svg",
+        position: "специалист по УМР, сотрудник деканата",
+    },
+];
